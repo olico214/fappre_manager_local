9ecc8d34a937ec60e9f33f0394a50ffe3514dd23
olico214 <oliverromero73@gmail.com>
2026-04-20 10:16:59 -0600

    cam,bios

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 import Dashboard from "./components/inicio";
 
 
-
 export default function Home() {
-  const url = 'http://51.210.246.210:3005/api'
-  console.log(url)
+  const urlback = process.env.URL
   return (
     <div>
-      <Dashboard url={url} />
+      <Dashboard url={urlback} />
     </div>
   )
 }
